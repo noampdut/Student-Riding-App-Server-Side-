fb3f6d2ad447943ce1c9487fe5c17eea383711d8
Noam Pdut <noampdut@gmail.com>
2023-03-14 17:25:09 +0200

    PassengerService updated.

--- trempApplication/Properties/Services/PassengerService.cs
+++ trempApplication/Properties/Services/PassengerService.cs
@@ -15,33 +15,82 @@ namespace trempApplication.Properties.Services
         }
         public async Task AddPassenger(Passenger passenger)
         {
-            await passengersCollection.InsertOneAsync(passenger);
+            try
+            {
+                if (passenger == null)
+                {
+                    throw new ArgumentNullException(nameof(passenger), "The passenger object is null.");
+                }
+
+                await passengersCollection.InsertOneAsync(passenger);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+                throw new Exception("Error adding passenger to database", ex);
+            }
         }
 
         public async Task DeletePassenger(Guid id)
         {
-            var filter = Builders<Passenger>.Filter.Eq(u => u.Id, id);
-            await passengersCollection.DeleteOneAsync(filter);
+            try
+            {
+                var filter = Builders<Passenger>.Filter.Eq(u => u.Id, id);
+                await passengersCollection.DeleteOneAsync(filter);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+                throw new Exception("Error deleting passenger from database", ex);
+            }
         }
 
         public async Task<List<Passenger>> GetAllPassengers()
         {
-            var passengers = await passengersCollection.Find(u => true).ToListAsync();
-            return passengers;
+            try
+            {
+                var passengers = await passengersCollection.Find(u => true).ToListAsync();
+                return passengers;
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+                throw new Exception("Error getting all passengers from database", ex);
+            }
         }
-
 
         public async Task<Passenger> GetPassengerById(Guid id)
         {
-            var filter = Builders<Passenger>.Filter.Eq(u => u.Id, id);
-            var passenger = await passengersCollection.Find(filter).FirstOrDefaultAsync();
-            return passenger;
+            try
+            {
+                var filter = Builders<Passenger>.Filter.Eq(u => u.Id, id);
+                var passenger = await passengersCollection.Find(filter).FirstOrDefaultAsync();
+                return passenger;
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+                throw new Exception("Error getting passenger by id from database", ex);
+            }
         }
 
         public async Task UpdatePassenger(Passenger passenger, Guid id)
         {
-            var filter = Builders<Passenger>.Filter.Eq(u => u.Id, id);
-            await passengersCollection.ReplaceOneAsync(filter, passenger);
+            try
+            {
+                if (passenger == null)
+                {
+                    throw new ArgumentNullException(nameof(passenger), "The passenger object is null.");
+                }
+
+                var filter = Builders<Passenger>.Filter.Eq(u => u.Id, id);
+                await passengersCollection.ReplaceOneAsync(filter, passenger);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+                throw new Exception("Error updating passenger in database", ex);
+            }
         }
     }
 }
